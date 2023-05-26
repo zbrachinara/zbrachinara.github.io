ced97cd5cc9e5195872cbcfcf9fe28fb0bfe8372
zbrachinara <zbrachinara@gmail.com>
2023-05-26 06:56:06 -0400

    move badges to center

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,22 @@
 <div class="flex flex-col">
 	<div class="flex h-screen">
 		<div class="my-auto mx-auto text-center space-y-10">
-			<h1 class="font-extrabold text-4xl">I watched Kaguya sama movie</h1>
-			<p class="">it was good</p>
-		</div>
-	</div>
-
-	<div class="flex px-10 py-10 flex-col">
-		<h1 class="font-bold text-xl mx-auto">About me</h1>
-		<div class="grid grid-cols-3 space-x-10">
-			<div class="flex flex-col">
-				<h1>Github</h1>
-				<p>Since 2020!</p>
-				<a href="https://wakatime.com/@d06087a9-1552-4763-b7f2-3957dcd1dd86"
-					><img
-						src="https://wakatime.com/badge/user/d06087a9-1552-4763-b7f2-3957dcd1dd86.svg"
-						alt="wakatime badge"
-					/></a
-				>
-			</div>
-			<div>
-				<h1>Youtube</h1>
-				<a href="https://www.youtube.com/@zbrachi">Channel</a>
-			</div>
-			<div>
-				<h1>Placeholder</h1>
+			<div class="flex px-10 py-10 flex-col">
+				<div class="grid grid-cols-2 space-x-10">
+					<div class="flex flex-col">
+						<h1>Github</h1>
+						<a href="https://wakatime.com/@d06087a9-1552-4763-b7f2-3957dcd1dd86"
+							><img
+								src="https://wakatime.com/badge/user/d06087a9-1552-4763-b7f2-3957dcd1dd86.svg"
+								alt="wakatime badge"
+							/></a
+						>
+					</div>
+					<div>
+						<h1>Youtube</h1>
+						<a href="https://www.youtube.com/@zbrachi">Channel</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
