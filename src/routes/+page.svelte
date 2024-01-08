8be3d5b3d07944863434eff1a49a9c46db9122b5
zbrachinara <zbrachinara@gmail.com>
2024-01-08 13:43:24 -0500

    grid layout on the front

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,23 @@
 <div class="flex flex-col">
 	<div class="flex h-screen">
-		<div class="my-auto mx-auto text-center space-y-10">
-			<div class="flex px-10 py-10 flex-col">
-				<div class="grid grid-cols-2 space-x-10">
-					<div class="flex flex-col">
-						<h1>Github</h1>
-						<a href="https://wakatime.com/@d06087a9-1552-4763-b7f2-3957dcd1dd86"
-							><img
-								src="https://wakatime.com/badge/user/d06087a9-1552-4763-b7f2-3957dcd1dd86.svg"
-								alt="wakatime badge"
-							/></a
-						>
+		<div class="my-auto mx-auto space-y-10 w-3/5 h-2/3">
+			<div class="bg-yellow-100 p-4 flex h-full">
+				<div class="flex-col w-1/6">
+					<div class="h-1/6">a</div>
+					<div class="h-1/3">a</div>
+					<div class="h-1/2">a</div>
+				</div>
+				<div class="flex-col w-1/3">
+					<div class="h-1/6">a</div>
+					<div class="h-1/3 bg-yellow-200">
+						<h1 class="text-6xl font-bold">HI,I'M<br>ERIC</h1>
 					</div>
-					<div>
-						<h1>Youtube</h1>
-						<a href="https://www.youtube.com/@zbrachi">Channel</a>
-					</div>
+					<div class="h-1/2">a</div>
+				</div>
+				<div class="flex-col w-1/2">
+					<div class="h-1/6">a</div>
+					<div class="h-1/3">a</div>
+					<div class="h-1/2">a</div>
 				</div>
 			</div>
 		</div>
@@ -31,3 +33,17 @@
 		<li>quicksweeper</li>
 	</ul>
 </div>
+
+<!-- <div class="flex flex-col">
+	<h1>Github</h1>
+	<a href="https://wakatime.com/@d06087a9-1552-4763-b7f2-3957dcd1dd86"
+		><img
+			src="https://wakatime.com/badge/user/d06087a9-1552-4763-b7f2-3957dcd1dd86.svg"
+			alt="wakatime badge"
+		/></a
+	>
+</div>
+<div>
+	<h1>Youtube</h1>
+	<a href="https://www.youtube.com/@zbrachi">Channel</a>
+</div> -->
